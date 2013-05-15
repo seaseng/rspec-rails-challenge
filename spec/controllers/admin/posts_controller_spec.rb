f47e0dda1f1c71f61342bed5bd0740e4b3f41f8b
Ed Shadi <edshadi@gmail.com>
2013-05-15 10:36:26 -0700

    remove the http_authentication, it threw most students and doesn't add a lot of value. I will replace it with regular auth.

--- spec/controllers/admin/posts_controller_spec.rb
+++ spec/controllers/admin/posts_controller_spec.rb
@@ -4,15 +4,6 @@ describe Admin::PostsController do
   describe "admin panel" do
     it "welcome page requires http authentication" do
       get :index
-      response.status.should eq 401
-    end
-
-    it "welcome page username is 'geek' and password is 'jock'" do
-      user = 'geek'
-      pw   = 'jock'
-      request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials(user,pw)
-
-      get :index
       response.status.should eq 200
     end
 
